05c7ce3e01490b31a1022e10dcd5e8abb4a5818c
hazarkir <kirhazar@gmail.com>
2023-11-22 03:17:59 +0300

    first commit

---
+++ Kronometre/Form1.Designer.vb
@@ -0,0 +1,106 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        Label1 = New Label()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        ListBox1 = New ListBox()
+        Timer1 = New Timer(components)
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 27.75F, FontStyle.Regular, GraphicsUnit.Point)
+        Label1.Location = New Point(97, 67)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(178, 50)
+        Label1.TabIndex = 0
+        Label1.Text = "00:00:000"
+        ' 
+        ' Button1
+        ' 
+        Button1.BackgroundImageLayout = ImageLayout.Zoom
+        Button1.Location = New Point(155, 454)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(40, 40)
+        Button1.TabIndex = 1
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.BackgroundImageLayout = ImageLayout.Zoom
+        Button2.Location = New Point(97, 454)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(40, 40)
+        Button2.TabIndex = 2
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.BackgroundImageLayout = ImageLayout.Zoom
+        Button3.Location = New Point(213, 454)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(40, 40)
+        Button3.TabIndex = 3
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' ListBox1
+        ' 
+        ListBox1.FormattingEnabled = True
+        ListBox1.ItemHeight = 15
+        ListBox1.Location = New Point(14, 240)
+        ListBox1.Name = "ListBox1"
+        ListBox1.Size = New Size(334, 184)
+        ListBox1.TabIndex = 4
+        ' 
+        ' Timer1
+        ' 
+        Timer1.Interval = 1
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(360, 538)
+        Controls.Add(ListBox1)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Timer1 As Timer
+End Class
---
+++ Kronometre/Form1.vb
@@ -0,0 +1,66 @@
+﻿Public Class Form1
+    Private kronometre As New Stopwatch
+    Dim play As Boolean = True
+
+
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Dim sure As TimeSpan = kronometre.Elapsed
+        Label1.Text = String.Format("{0:00}:{1:00}:{2:000}", sure.Minutes, sure.Seconds, sure.Milliseconds)
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Button2.Enabled = False
+
+        kronometre.Reset()
+        Label1.Text = "00:00:000"
+        ListBox1.Items.Clear()
+
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If play = True Then
+            Timer1.Start()
+            kronometre.Start()
+
+            play = False
+            Button1.BackgroundImage = Image.FromFile("img/pause.png")
+            Button2.Enabled = False
+            Button3.Enabled = True
+
+        Else
+            Timer1.Stop()
+            kronometre.Stop()
+
+            play = True
+            Button1.BackgroundImage = Image.FromFile("img/play.png")
+            Button2.Enabled = True
+            Button3.Enabled = False
+
+        End If
+
+
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Button1.BackgroundImage = Image.FromFile("img/play.png")
+        Button2.BackgroundImage = Image.FromFile("img/replay.png")
+        Button3.BackgroundImage = Image.FromFile("img/clock.png")
+
+        Button2.Enabled = False
+        Button3.Enabled = False
+
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        ListBox1.Items.Add(ListBox1.Items.Count + 1 & ". TUR -- " & Label1.Text)
+    End Sub
+End Class
